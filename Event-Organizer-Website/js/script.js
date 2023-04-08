ffa4b7bf36004716b7815b25e06154ef551cbe68
FelizardoCaculo <felizardo1987@gmail.com>
2023-04-08 21:40:41 +0100

    fixing layout issues

--- Event-Organizer-Website/js/script.js
+++ Event-Organizer-Website/js/script.js
@@ -1,17 +1,13 @@
 const menu = document.querySelector('#menu-bars');
 const navbar = document.querySelector('.navbar');
-
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.toggle('active');
 }
-
-
 var swiper = new Swiper(".home-slider", {
     effect: "coverflow",
     grabCursor: true,
@@ -29,9 +25,7 @@ var swiper = new Swiper(".home-slider", {
         delay: 3000,
         disableOnIteration: false,
     }
-
 });
-
 var swiper = new Swiper(".review-slider", {
     slidesPerView: 1,
     grabCursor: true,
